b537303127025ba1bf1dd3a1ee3609744b48ab55
Rincla971 <reginor971@gmail.com>
2023-10-11 13:58:46 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <strings.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str1[] = "\"Programming is like building a multilingual puzzle";
+puts(str1);
+return (0);
+}
