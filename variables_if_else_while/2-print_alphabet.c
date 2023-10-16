a4b6667e0c769c994fa34e6053afff959c1c17b8
Rincla971 <reginor971@gmail.com>
2023-10-16 04:31:36 -0700

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+char lettre;
+
+for (lettre = 'a'; lettre <= 'z'; lettre++)
+{
+putchar(lettre);
+};
+putchar('\n');
+return (0);
+}
