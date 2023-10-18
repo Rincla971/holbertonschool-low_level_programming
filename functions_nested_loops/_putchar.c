99d2a85724993f18b5b4653c21fb7c153f098e8f
Rincla971 <reginor971@gmail.com>
2023-10-18 02:31:24 -0700

    1-alphabet

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 #include <unistd.h>
 
 /**
@@ -9,8 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c);
-
-void print_alphabet(void);
-
-#endif
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
