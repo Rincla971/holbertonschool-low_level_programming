cd76b4478203922a09c442cb6db18a8d4b337b4f
Rincla971 <reginor971@gmail.com>
2023-10-29 08:41:01 -0700

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,46 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * leet - leet encoder
- * @str: string to be encoded
- *
- * Return: address of the encoded string
+ * leet - entry point
+ * @s: pointer
+ * Description: function that encodes a string into 1337.
+ * Return: convert string if success
  */
-
-char *leet(char *str)
+char *leet(char *s)
 {
-	int i = 0;
+int i, j;
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
 
-	while (str[i] != '\0')
-	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (str);
+for (i = 0; *(s + i); i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (a[j] == s[i])
+s[i] = b[j];
 }
-
-/**
- * transform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char x)
-{
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 8)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i])
-		{
-			replacement = i + '0';
-			break;
-		}
-		i++;
-	}
-	return (replacement);
+}
+return (s);
 }
