2e41a21e771524b8771c27f9d65720615e97c94d
Rincla971 <reginor971@gmail.com>
2023-10-29 11:29:42 -0700

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,46 +1,25 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _strspn - gets the length of a prefix substring
- * @s: string to be searched
- * @accept: string to be used
- *
- * Return: number of bytes in the initial segment of s which are part of accept
+ * _strspn - entry point
+ * @s: pointer intial segment string
+ * @accept: new segment string
+ * Description: function that gets the length of a prefix substring.
+ * Return: Returns the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int match = 0;
-
-	while (s[i] != '\0')
-	{
-		if (_strchr(accept, s[i]) == NULL)
-			break;
-		match++;
-		i++;
-	}
-
-	return (match);
-}
-
-/**
- * _strchr - locates a char in a string
- * @s: string to be searched
- * @c: char to be checked
- *
- * Return: pointer to the first occurence of c in s
- */
-
-char *_strchr(char *s, char c)
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
 {
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++);
-
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+break;
+}
+if (accept[j] == '\0')
+return (i);
+}
+return (i);
 }
